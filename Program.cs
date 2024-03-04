86a87789de1fe26eec0a846061d8e339eb3a79ab
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-04 21:04:16 +0500

    Menu Write

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+bool isCantinue = true;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Welcome to, Phone Book");
+    Console.WriteLine("1. Create phone book");
+    Console.WriteLine("2. Update phone book");
+    Console.WriteLine("3. Delete one phone book");
+    Console.WriteLine("4. Read all phone book");
+    Console.WriteLine("5. Read one phone book");
+
+    Console.Write("Enter your command(1,2,3...): ");
+    int command = Convert.ToInt32(Console.ReadLine());
+
+    if (command == 1)
+    {
+        Console.WriteLine("Not Impliment Create phone book");
+    }
+    if(command == 2)
+    {
+        Console.WriteLine("Not Impliment Update phone book");
+    }
+    if (command == 3)
+    {
+        Console.WriteLine("Not Impliment Delete one phone book");
+    }
+    if (command == 4)
+    {
+        Console.WriteLine("Not Impliment Read all phone book");
+    }
+    if (command == 5)
+    {
+        Console.WriteLine("Not Impliment Read one phone book");
+    }
+
+    Console.Write("Is your cantinue(yes / no): ");
+    string nextCount = Console.ReadLine();
+
+    if (nextCount.ToString().ToLower().Contains("no"))
+    {
+        isCantinue = false;
+    }
+
+} while (isCantinue);
