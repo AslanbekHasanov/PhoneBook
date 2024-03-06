fa0ff2ccfdcac632ed07563b81a2eaaf5ce40106
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-06 19:54:14 +0500

    Validate Against Null Input

--- Services/Contacts/ContactService.cs
+++ Services/Contacts/ContactService.cs
@@ -13,14 +13,19 @@ namespace PhoneBook.Services.Contacts
     {
         private readonly IStorageBroker storageBroker;
         private readonly ILoggingBroker loggingBroker;
+
         public ContactService()
         {
             this.storageBroker = new FileStorageBroker();
             this.loggingBroker = new LoggingBroker();
         }
 
-        public Contact AddContact(Contact contact) =>
-            storageBroker.AddContact(contact);
+        public Contact AddContact(Contact contact)
+        {
+            return contact is null
+                ? CreateAndLogInvalidContact() 
+                : this.storageBroker.AddContact(contact);
+        }
 
         public void SowContacts()
         {
@@ -32,6 +37,12 @@ namespace PhoneBook.Services.Contacts
             }
 
             this.loggingBroker.LogInformation($"=== End of contacts ===");
+        }
+
+        private Contact CreateAndLogInvalidContact()
+        {
+            this.loggingBroker.LogError("Contact is invaled");
+            return new Contact();
         }
     }
 
